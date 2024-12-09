7894eeb94dae0dde65ecdc2a6c0cc83a4450ce63
Anandaram R <anandaramr1915@gmail.com>
2024-12-09 21:15:52 +0530

    Add support for input files

--- Main.java
+++ Main.java
@@ -2,27 +2,49 @@
 import Interpreter.Interpreter;
 import Lexer.Lexer;
 import Lexer.Token;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.List;
 import java.util.Scanner;
 
 public class Main {
+    static int lineCount = 0;
 
-    public static void main(String args[]) {
+    public static void main(String[] args) throws IOException {
+        if(args.length==0) runREPL();
+        else {
+            try (BufferedReader input = new BufferedReader(new FileReader(args[0]))) {
+                while (true) {
+                    lineCount++;
+                    String line = input.readLine();
+                    if(line==null) break;
+                    run(line);
+                }
+            }
+        }
+    }
+
+    private static void runREPL() {
         Scanner scan = new Scanner(System.in);
 
-        while (true) { 
+        while (true) {
             System.out.print("> ");
             String line = scan.nextLine();
-
             if(line.equals("quit")) break;
 
-            Lexer lexer = new Lexer();
-            List<Token> tokens = lexer.tokenize(line);
-            
-            Interpreter inter = new Interpreter();
-            inter.execute(tokens);
+            run(line);
         }
 
         scan.close();
+    }
+
+    private static void run(String line) {
+        Lexer lexer = new Lexer();
+        List<Token> tokens = lexer.tokenize(line, lineCount);
+
+        Interpreter inter = new Interpreter();
+        inter.execute(tokens);
     }
 }
